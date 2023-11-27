848c02342be1af616ecc94f288eb32d3f74c13a5
R4 <81534084+R4z4kk@users.noreply.github.com>
2023-11-27 23:55:13 +0100

    feat : add slogan to home page
    add picture with slogan on home page

---
+++ src/components/IndexPageSlogan.vue
@@ -0,0 +1,11 @@
+<template lang="fr">
+  <q-img height="200px" src="../assets/wallpaperflare.com_wallpaper.jpg">
+    <div class="absolute-full text-subtitle2 flex flex-center">
+      "Future start tommorow"
+    </div>
+  </q-img>
+</template>
+<script>
+export default {};
+</script>
+<style lang="fr"></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-page class="flex row justify-center items-start q-mx-md">
+  <q-page style="min-height: 0px" class="flex row justify-evenly items-start">
+    <IndexPageSlogan />
     <IndexPageFirstCards />
   </q-page>
 </template>
@@ -7,10 +8,12 @@
 <script>
 import { defineComponent } from "vue";
 import IndexPageFirstCards from "../components/IndexPageFirstCards.vue";
+import IndexPageSlogan from "src/components/IndexPageSlogan.vue";
 export default defineComponent({
   name: "IndexPage",
   components: {
     IndexPageFirstCards,
+    IndexPageSlogan,
   },
 });
 </script>

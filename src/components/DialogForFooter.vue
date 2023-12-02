b20fe5e70bb2a67fc077133407dc8d933b95b414
R4 <81534084+R4z4kk@users.noreply.github.com>
2023-12-02 14:29:55 +0100

    feat : add footer + contact dialog to MainLayout
    footer now appears on everypage and when you click the underline text, a dialog appears

---
+++ src/components/DialogForFooter.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-dialog v-model="footerDialogState">
+    <q-card>
+      <q-card-section class="flex row justify-between q-ml-md">
+        <div class="text-h6">Formulaire de contact :</div>
+        <div class="justify-end">
+          <q-btn icon="close" flat round dense v-close-popup />
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="q-px-md" style="max-width: 600px">
+          <q-form
+            @submit="onSubmit"
+            @reset="onReset"
+            class="q-gutter-md flex row justify-center"
+          >
+            <q-input
+              outlined
+              class="col-12"
+              v-model="nickname"
+              label="Nom"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Renseignez votre nom',
+              ]"
+            />
+            <q-input
+              outlined
+              class="col-12"
+              v-model="email"
+              label="Email"
+              lazy-rules
+              :rules="[
+                (val) => val.length > 0 || 'Renseignez votre email',
+                (val) =>
+                  (val && emailRegex.test(val)) ||
+                  'Renseignez une adresse valide',
+              ]"
+            />
+
+            <q-input
+              outlined
+              class="col-12"
+              style="width: 500px"
+              type="textarea"
+              v-model="message"
+              label="Votre message"
+              lazy-rules
+              required
+              :rules="[
+                (val) => (val && val.length > 0) || 'Écrivez votre message',
+              ]"
+            />
+
+            <!-- <q-toggle v-model="accept" label="I accept the license and terms" /> -->
+
+            <div style="text-align: right">
+              <q-btn label="Envoyer" type="submit" color="primary" />
+              <!-- <q-btn
+                label="Reset"
+                type="reset"
+                color="primary"
+                flat
+                class="q-ml-sm"
+              /> -->
+            </div>
+          </q-form>
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      footerDialogState: false,
+      nickname: "",
+      message: "",
+      email: "",
+    };
+  },
+  props: {
+    showDialog: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    onSubmit() {},
+    onReset() {
+      this.nickname = "";
+      this.message = "";
+    },
+  },
+};
+</script>
+<style></style>
--- src/components/FooterForLayer.vue
+++ src/components/FooterForLayer.vue
@@ -1,15 +1,26 @@
 <template>
+  <DialogForFooter v-model="footerDialog" :showDialog="footerDialog" />
   <q-footer elevated>
     <q-toolbar>
       <q-toolbar-title>
         <pre
           style="text-align: center"
-        >Une question ? Un avis ? <u>Contactez-moi</u></pre>
+        >Une question ? Un avis ? <u @click="footerDialog = !footerDialog">Contactez-moi</u></pre>
       </q-toolbar-title>
     </q-toolbar>
   </q-footer>
 </template>
 <script>
-export default {};
+import DialogForFooter from "./DialogForFooter.vue";
+export default {
+  data() {
+    return {
+      footerDialog: false,
+    };
+  },
+  components: {
+    DialogForFooter,
+  },
+};
 </script>
 <style></style>

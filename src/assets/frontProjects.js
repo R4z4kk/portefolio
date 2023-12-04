ad11a0095c2178c0c4f04d9fbb9a0effb3438af9
R4 <81534084+R4z4kk@users.noreply.github.com>
2023-12-04 23:54:53 +0100

    test : add constant for projects page / front
    add constant to test the future rendering

---
+++ src/assets/frontProjects.js
@@ -0,0 +1,47 @@
+const frontProjects = [
+  {
+    title: "Portefolio",
+    tags: ["portefolio", "quasar", "javascript", "express"],
+    description: "Un outil pour me présenter et présenter mes projets.",
+    sourcePhoto: "../assets/questionMark.jpg",
+    siteLink: "https://test",
+    GitLink: "none",
+    technologies: ["quasar", "javascript", "express"],
+    goals: [
+      "Me présenter",
+      "Présenter mes capacités",
+      "Présenter mes réalisations",
+    ],
+    keyFeatures: [
+      "Rendu déclaratif et conditionnel",
+      `Gestion d'évènements`,
+      "Programmation orienté objets / composants",
+      `Envois et réceptions d'évènements entre composants`,
+      "Serveur express",
+    ],
+    implementation: [
+      { what: "", why: "", how: "" },
+      { what: "", why: "", how: "" },
+    ],
+    problematics: [{ problematic: "none", resolution: "none" }],
+    optimizations: ["opti 1", "opti 2"],
+  },
+  {
+    title: "Blog",
+    tags: ["Blog", "react", "javascript", "express"],
+    description: "Un outil pour me présenter et présenter mes projets.",
+    sourcePhoto: "../assets/questionMark.jpg",
+    siteLink: "https://test",
+    GitLink: "none",
+    technologies: ["quasar", "javascript", "express"],
+    goals: ["Vous tenir à jour sur mon parcours", "Présenter mon réalisations"],
+    keyFeatures: ["test1", `test2`],
+    implementation: [
+      { what: "", why: "", how: "" },
+      { what: "", why: "", how: "" },
+    ],
+    problematics: [{ problematic: "none", resolution: "none" }],
+    optimizations: ["opti 1", "opti 2"],
+  },
+];
+s;
